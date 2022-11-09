d92fa08d5ca21cdf2ea1b2d154eb59a4844b7253
Gautam Sagar <gautamsagar.mallela99@gmail.com>
2022-11-09 11:29:57 -0600

    tried a post request to add data into mongodb

--- controllers/tree.js
+++ controllers/tree.js
@@ -18,8 +18,24 @@ exports.tree_detail = function(req, res) {
 }; 
  
 // Handle tree create on POST. 
-exports.tree_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: tree create POST'); 
+exports.tree_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new tree(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.tree_species = req.body.tree_species; 
+    document.tree_age = req.body.tree_age; 
+    document.tree_height = req.body.tree_height; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }  
 }; 
  
 // Handle tree delete form on DELETE. 
