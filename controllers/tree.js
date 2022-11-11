35fb39b626c9ff8a44f57df4d108cf4f4ff365e3
Gautam Sagar <gautamsagar.mallela99@gmail.com>
2022-11-10 19:44:39 -0600

    Assignment 11 with single id reqest message

--- controllers/tree.js
+++ controllers/tree.js
@@ -13,9 +13,16 @@ exports.tree_list = async function(req, res) {
 }; 
  
 // for a specific tree. 
-exports.tree_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: tree detail: ' + req.params.id); 
-}; 
+exports.tree_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await tree.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
  
 // Handle tree create on POST. 
 exports.tree_create_post = async function(req, res) { 
--- routes/tree.js
+++ routes/tree.js
@@ -5,4 +5,6 @@ var router = express.Router();
 /* GET trees */ 
 router.get('/', tree_controlers.tree_view_all_Page ); 
 
+// GET request for one costume.
+router.get('/trees/:id', tree_controlers.tree_detail);
 module.exports = router;
